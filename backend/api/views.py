f4ad40ae4df21d2715c61d502e5ffd44708a2dd0
Szymon Dębski <skwdebski@gmail.com>
2023-07-21 14:35:17 +0200

    working version

--- backend/api/views.py
+++ backend/api/views.py
@@ -16,7 +16,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        token["email"] = user.email
+        token["username"] = user.username
 
         return token
 
