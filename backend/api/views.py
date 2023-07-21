5c3a803f73ce2245b5025240bb579e4fa18c2970
Szymon Dębski <skwdebski@gmail.com>
2023-07-21 14:51:32 +0200

    user restructured

--- backend/api/views.py
+++ backend/api/views.py
@@ -16,7 +16,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        token["username"] = user.username
+        token["email"] = user.email
 
         return token
 
